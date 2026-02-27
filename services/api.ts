3cb46badd9ed97c5285f2c6e7a0b6427da1e0658
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2026-02-27 14:05:24 +0100

    fix: update API URL fallback to api.escapemaster.es

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://manager.escapemaster.es/api";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.escapemaster.es";
 
 const api = axios.create({
   baseURL: API_URL,
